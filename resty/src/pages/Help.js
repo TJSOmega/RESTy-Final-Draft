764ef5aa3c96b7ddac4f32cf5eecda1de06e9eb4
Terrence Jamal Simms <tjs@omegacc.com>
2021-05-23 13:20:54 -0700

    Started rebuild of RESTy

---
+++ resty/src/pages/Help.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Help extends React.Component {
+
+    render() {
+        return (
+            <div>Help will live here</div>
+        );
+
+    }
+}
+
+
+export default Help
---
+++ resty/src/pages/History.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class History extends React.Component {
+
+    render() {
+        return (
+            <div>History will live here</div>
+        );
+
+    }
+}
+
+export default History
